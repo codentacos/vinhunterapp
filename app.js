17c1d3602d011d582fc8630bde780a6b319d2b37
Zachary Nelson <nelsonz2013@hotmail.com>
2020-02-04 20:33:08 -0600

    added project to new repo, api keys removed

---
+++ app.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+
+var request = require('request'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    apiKey = process.env.RAPID_API_KEY;
+    app = express();
+
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+// ROUTES
+app.get('/', function(req, res) {
+    res.redirect('search');
+});
+
+// SEARCH VIN ROUTE
+app.get('/search', function(req, res) {
+    res.render('search');
+});
+
+// SHOW CAR DATA ROUTE
+app.get('/car', function(req, res) {
+    var vinData = req.query.vindata;
+    var options = {
+        method: 'GET',
+        url: 'https://vindecoder.p.rapidapi.com/decode_vin',
+        qs: {vin: vinData},
+        headers: {
+            'x-rapidapi-host': 'vindecoder.p.rapidapi.com',
+            'x-rapidapi-key': apiKey
+          }
+    };
+
+    request(options, function(err, response, body) {
+        if (!err && response.statusCode === 200) {
+            const data = JSON.parse(body);
+            res.render('car', { data: data });
+        } else {
+            console.log('Error: ' + err);
+                res.redirect('/error');
+        }
+    });
+    // res.render('car');
+});
+
+app.get('/error', function(req, res) {
+    res.render('error');
+})
+
+app.listen(3000, function() {
+    console.log('Server on port 3000');
+});
